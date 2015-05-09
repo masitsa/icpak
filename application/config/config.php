c5f2a593bce5cbd360ff461fd15e20412f3924e7
Alvaro Masitsa <alvaromasitsa104@gmail.com>
2015-05-09 13:09:47 +0300

    Registration & login

--- application/config/config.php
+++ application/config/config.php
@@ -14,8 +14,8 @@
 | path to your installation.
 |
 */
-$config['base_url']	= 'http://localhost/icpak/';
-//$config['base_url']	= 'http://sithdimension.com/icpak/';
+//$config['base_url']	= 'http://localhost/icpak/';
+$config['base_url']	= 'http://sixthdimensions.com/icpak/';
 
 /*
 |--------------------------------------------------------------------------
@@ -225,7 +225,7 @@ $config['cache_path'] = '';
 | MUST set an encryption key.  See the user guide for info.
 |
 */
-$config['encryption_key'] = 'www.instorelook.com.au';
+$config['encryption_key'] = 'www.sixthdimensions.com';
 
 /*
 |--------------------------------------------------------------------------
@@ -249,7 +249,7 @@ $config['sess_cookie_name']		= 'ci_session';
 $config['sess_expiration']		= 7200;
 $config['sess_expire_on_close']	= FALSE;
 $config['sess_encrypt_cookie']	= FALSE;
-$config['sess_use_database']	= FALSE;
+$config['sess_use_database']	= TRUE;
 $config['sess_table_name']		= 'ci_sessions';
 $config['sess_match_ip']		= FALSE;
 $config['sess_match_useragent']	= TRUE;
